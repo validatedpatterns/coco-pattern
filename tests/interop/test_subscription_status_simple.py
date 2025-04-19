fe2ae19286ded55d35025ce2f0484a261e2620ca
Chris Butler <chris.butler@redhat.com>
2025-04-19 10:36:05 +1000

    feat: add compatible qe tests
    Signed-off-by: Chris Butler <chris.butler@redhat.com>

---
+++ tests/interop/test_subscription_status_simple.py
@@ -0,0 +1,31 @@
+import logging
+
+import pytest
+from validatedpatterns_tests.interop import subscription
+
+from . import __loggername__
+
+logger = logging.getLogger(__loggername__)
+
+
+@pytest.mark.subscription_status_hub
+def test_subscription_status(openshift_dyn_client):
+    # These are the operator subscriptions and their associated namespaces
+    expected_subs = {
+        "openshift-gitops-operator": ["openshift-operators"],
+        "advanced-cluster-management": ["open-cluster-management"],
+        "multicluster-engine": ["multicluster-engine"],
+        "sandboxed-containers-operator": ["openshift-sandboxed-containers-operator"],
+        "trustee-operator": ["trustee-operator-system"],
+        "openshift-cert-manager-operator": ["cert-manager-operator"],
+
+    }
+
+    err_msg = subscription.subscription_status(
+        openshift_dyn_client, expected_subs, diff=True
+    )
+    if err_msg:
+        logger.error(f"FAIL: {err_msg}")
+        assert False, err_msg
+    else:
+        logger.info("PASS: Subscription status check passed")
