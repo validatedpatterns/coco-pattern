966d426e0b020a99b79e823f8d95652565cd34a0
Mark LaBonte <mlabonte@redhat.com>
2025-04-24 16:54:04 -0400

    Add support file for QE tests

---
+++ tests/interop/run_tests.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+export EXTERNAL_TEST="true"
+export PATTERN_NAME="CoCoPattern"
+export PATTERN_SHORTNAME="coco"
+
+if [ -z "${KUBECONFIG}" ]; then
+    echo "No kubeconfig file set for hub cluster"
+    exit 1
+fi
+
+if [ -z "${INFRA_PROVIDER}" ]; then
+    echo "INFRA_PROVIDER is not defined"
+    exit 1
+fi
+
+if [ -z "${WORKSPACE}" ]; then
+    export WORKSPACE=/tmp
+fi
+
+pytest -lv --disable-warnings test_subscription_status_hub.py --kubeconfig $KUBECONFIG --junit-xml $WORKSPACE/test_subscription_status_hub.xml
+
+pytest -lv --disable-warnings test_validate_hub_site_components.py --kubeconfig $KUBECONFIG --junit-xml $WORKSPACE/test_validate_hub_site_components.xml
+
+python3 create_ci_badge.py
